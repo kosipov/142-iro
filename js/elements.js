2e1c671114743a32e52076e78ef900d7a016d9c1
kosipov <mail@kosipov.ru>
2023-10-27 19:31:42 +0300

    release search on list

---
+++ js/elements.js
@@ -0,0 +1,14 @@
+export const azsTable = () =>
+  `<thead>
+      <tr>
+        <th>Адрес</th>
+        <th>АИ-92</th>
+        <th>АИ-92СПЕЦ</th>
+        <th>АИ-95</th>
+        <th>АИ-95СПЕЦ</th>
+        <th>АИ-98</th>
+        <th>АИ-98СПЕЦ</th>
+        <th>ДТСПЕЦ</th>
+        <th>ДТ</th>
+      </tr>
+      </thead>`;
--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 import {JSON} from "./resource.js";
+import {azsTable} from "./elements.js";
 
 const body = document.querySelector(".body");
-const table = document.querySelector(".body__table");
+const search = body.querySelector(".body__table__search");
+const table = body.querySelector(".body__table");
 const createTableCell = (value) => `<td>${value}</td>`;
 
-const initTable = () => {
-  JSON.forEach((value) => {
+const initTable = (values) => {
+  initEmptyTable();
+  values.forEach((value) => {
     let insertElement = createTableCell(value.address + value.name)
+    let arr = [];
     let fuelElements = ``;
     value.fuels.forEach((fuelValue) => {
       fuelElements += createTableCell(fuelValue.price)
@@ -20,4 +24,19 @@ const initTable = () => {
   });
 };
 
-initTable();
+const filterTable = (searchValue) => {
+  const table = body.querySelector(".body__table");
+  initEmptyTable();
+  const foundValues = JSON.filter((value) => value.address.includes(searchValue) || value.name.includes(searchValue));
+  initTable(foundValues);
+}
+
+const initEmptyTable = () => {
+  table.replaceChildren('');
+  table.insertAdjacentHTML('beforeend', azsTable())
+}
+
+
+initTable(JSON);
+search.addEventListener('input', (event) => filterTable(event.target.value));
+
